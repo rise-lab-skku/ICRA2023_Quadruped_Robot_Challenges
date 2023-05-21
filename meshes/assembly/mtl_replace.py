607f053cd74c8a45089d9d2bda2232575bf8a8a8
Jeongmin Jeon <nicky707@hanmail.net>
2023-05-21 11:05:59 +0900

    add: objs

--- meshes/assembly/mtl_replace.py
+++ meshes/assembly/mtl_replace.py
@@ -9,11 +9,12 @@ for mtl in mtl_files:
     with open(mtl, 'r') as f:
         lines = f.readlines()
         
-        if 'map_Kd' in lines[-1]:
+        if 'map_Kd' in lines[-1] and 'nicky' in lines[-1]:
             # remove strings before the 'maps' in the last line
-            #lines[-1] = 'map_Kd maps/' + lines[-1].split('maps\\\\')[1]
+            print(lines[-1])
+            lines[-1] = 'map_Kd maps/' + lines[-1].split('maps\\\\')[1]
             lines[-1] = lines[-1].replace('_8.', '.')
-            
+            print(lines[-1])
             # write to file
             with open(mtl, 'w') as f:
                 f.writelines(lines)
